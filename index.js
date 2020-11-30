aceda3f58afb6ebaa06b72be5f6394579c831ff6
jpbatista <joao.batista@a.ficr.edu.br>
2020-11-30 16:33:51 -0300

    Configuração do database ok

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const express = require("express");
 const app = express();
 let ejs = require('ejs');
 const bodyParser = require('body-parser')
-
+//Database
+const connection = require('./database/database')
+//Connection
+.authenticate()
+.then(()=>{
+    console.log("Conexão ok")
+})
+.catch((msgErro)=>{
+    console.log("msgErro")
+})
 //Uso do express como view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));//Utilização de arquivos estáticos.
 app.use(bodyParser.json());//permite lermos dados do formulário via json, muito usado com API.
 app.use(bodyParser.urlencoded({extended:false}))//Decodifica os dados enviados pelo formulário.
 
-//ROTAS
+/**********ROTAS**********/
+
 app.get("/",(req,res)=>{
     res.render("index")//Não precisa do caminho porque o ejs vai direto na pasta view.
 });
@@ -29,7 +39,7 @@ app.post("/salvarPerguntas",(req,res)=>{
 
 
 
-
+//Servidor
 app.listen(8080,()=>{
     console.log("Servidor OK")//Configuração para o console.
 })
