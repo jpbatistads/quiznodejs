39ace366b9340993f8f052b70c3ff882322fec65
jpbatista <joao.batista@a.ficr.edu.br>
2020-11-30 08:45:02 -0300

    Dados sendo enviados para o back end falta salvar na base de dados

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 let ejs = require('ejs');
+const bodyParser = require('body-parser')
 
 //Uso do express como view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));//Utilização de arquivos estáticos.
+app.use(bodyParser.json());//permite lermos dados do formulário via json, muito usado com API.
+app.use(bodyParser.urlencoded({extended:false}))//Decodifica os dados enviados pelo formulário.
 
-
-
+//ROTAS
 app.get("/",(req,res)=>{
     res.render("index")//Não precisa do caminho porque o ejs vai direto na pasta view.
 });
@@ -17,6 +19,8 @@ app.get("/perguntar",(req,res)=>{
 });
 
 app.post("/salvarPerguntas",(req,res)=>{
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
     res.send("Formulário Receido")//Apontar rota no frontend no action do formulário.
 });
 
